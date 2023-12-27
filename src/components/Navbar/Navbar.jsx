711efc2185629907d2a200a5711f23e53e6b11b9
younuskhan1 <md.younuskhan412@gmail.com>
2023-12-27 18:41:50 +0600

    navbar has been responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,26 +7,29 @@ const Navbar = () => {
             <nav className="navbar-parents">
                 <div className="logo-and-club-name-div">
                      <img className="reader-logo" src={image} alt="" />
-                     <p className="club-name">ONLINE BOOKWORM CLUB</p>
+                     <div className="library-name-country">
+                        <p className="club-name">Online Bookworm Club</p>
+                        <p className="club-location">Frankfurt, Germany.</p>
+                     </div>
                 </div>
                 <div className="reader-introduction">
                     <div><img className="reader-image" src={readerImage} alt="" /></div>
                     <div>
                         <p className="reader-name">Hasan Mahmud Khan</p>
-                        <p className="reader-location"><i className="fa-solid fa-location-dot"></i> Copenhagen, Denmark</p>
+                        <p className="reader-location"><i className="fa-solid fa-location-dot"></i> Copenhagen, Denmark.</p>
                     </div>
                 </div>
                 <div className="reader-condition">
                     <div className="single-info-div">
-                        <p><span className="weight-kg">77</span><span className="unit-measure">kg</span></p>
+                        <p><span className="kg-fts-yrs">77</span><span className="unit-measure">kgs</span></p>
                         <p className="measurement">weight</p>
                     </div>
                     <div className="single-info-div">
-                        <p><span className="weight-kg">6</span><span className="unit-measure">fts</span></p>
+                        <p><span className="kg-fts-yrs">6.2</span><span className="unit-measure">fts</span></p>
                         <p className="measurement">height</p>
                     </div>
                     <div className="single-info-div">
-                        <p><span className="weight-kg">28</span><span className="unit-measure">yrs</span></p>
+                        <p><span className="kg-fts-yrs">28</span><span className="unit-measure">yrs</span></p>
                         <p className="measurement">age</p>
                     </div>
                 </div>
