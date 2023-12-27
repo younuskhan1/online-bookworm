589198a5c122939dcbb355794ebebea0654210fa
younuskhan1 <md.younuskhan412@gmail.com>
2023-12-27 14:47:53 +0600

    navbar is about completed but responsive is left yet

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,6 +16,20 @@ const Navbar = () => {
                         <p className="reader-location"><i className="fa-solid fa-location-dot"></i> Copenhagen, Denmark</p>
                     </div>
                 </div>
+                <div className="reader-condition">
+                    <div className="single-info-div">
+                        <p><span className="weight-kg">77</span><span className="unit-measure">kg</span></p>
+                        <p className="measurement">weight</p>
+                    </div>
+                    <div className="single-info-div">
+                        <p><span className="weight-kg">6</span><span className="unit-measure">fts</span></p>
+                        <p className="measurement">height</p>
+                    </div>
+                    <div className="single-info-div">
+                        <p><span className="weight-kg">28</span><span className="unit-measure">yrs</span></p>
+                        <p className="measurement">age</p>
+                    </div>
+                </div>
             </nav>
         </>
     );
