10dd76d2de8c137af45ec70afc8cd452e15c4726
younuskhan1 <md.younuskhan412@gmail.com>
2023-12-28 12:03:28 +0600

    website layout is about completed but responsive is left yet

--- src/components/ReadingInfo/ReadingInfo.jsx
+++ src/components/ReadingInfo/ReadingInfo.jsx
@@ -5,7 +5,24 @@ const ReadingInfo = () => {
         <div className="reading-info-parent">
             <p style={{textAlign: "center",padding: "0px 10px 20px", fontSize: "25px",fontWeight: "700"}}>Break & Exercise Time</p>
             <div  className="reading-time-info">
-                <h3 style={{fontSize: "20px", color: "purple",borderBottom:"4px solid white", paddingBottom: "20px"}}>Add a Break</h3>
+                <h3 style={{fontSize: "20px", color: "purple",borderBottom:"4px solid white", paddingBottom: "20px",textAlign: "center"}}>Add a Break</h3>
+                <div className="break-time-button-parent">
+                    <button className="break-time-button"><span style={{fontSize: "22px",fontWeight:"700"}}>10</span>s</button>
+                    <button className="break-time-button button-margin"><span style={{fontSize: "22px",fontWeight:"700"}}>20</span>s</button>
+                    <button className="break-time-button button-margin"><span style={{fontSize: "22px",fontWeight:"700"}}>30</span>s</button>
+                    <button className="break-time-button button-margin"><span style={{fontSize: "22px",fontWeight:"700"}}>40</span>s</button>
+                    <button className="break-time-button button-margin"><span style={{fontSize: "22px",fontWeight:"700"}}>50</span>s</button>  
+                </div>
+                <h3 style={{fontSize: "20px", color: "purple", padding: "20px 0px",textAlign:"center",borderBottom:"4px solid white"}}>Exercise Details</h3>
+                <div className="exercise-time">
+                    <p style={{fontWeight: "700"}}>Exercise Time</p>
+                    <p style={{color: "gray"}}><span>200</span> seconds</p>
+                </div>
+                <div className="exercise-time">
+                    <p style={{fontWeight: "700"}}>Break Time</p>
+                    <p style={{color: "gray"}}><span>15</span> seconds</p>
+                </div>
+                <div style={{padding: "20px 0px"}}><button className="activity-completed-button">Activity Completed</button></div>
             </div>
         </div>
     );
